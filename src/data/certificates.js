4c513ad8f40886802b3db8e271f25593aac6a7ee
mohamed ehab <156261263+MOHAmED3467@users.noreply.github.com>
2025-11-29 12:51:34 +0200

    Refactor certificates data with updated details
    Updated certificate details including issuer, date, duration, and description for Python Programming Basics, IoT, and Frontend Development - Vue.js.

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -2,24 +2,24 @@ export const certificates = [
   {
     title: "Python Programming Basics",
     issuer: "ITI – Mahara-Tech",
-<<<<<<< HEAD
+
     image: new URL("../assets/certificates/python.png", import.meta.url).href,
-=======
+
     date: "October 18, 2025",
     duration: "1 hour 38 minutes",
     description:
       "Completed an introductory course covering the fundamentals of Python programming, problem-solving, and practical coding skills.",
     image: new URL("src/assets/certificates/python.png", import.meta.url).href,
     link: "#",
->>>>>>> 69650f0bb3eb81901eaa0c784099c7f62a657a73
+
   },
 
   {
     title: "Internet of Things (IoT)",
-<<<<<<< HEAD
+
     issuer: "Information Technology Institute",
     image: new URL("../assets/certificates/iot.png", import.meta.url).href,
-=======
+
     issuer: "Information Technology Institute (ITI)",
     date: "July 6 – July 10, 2025",
     duration: "30 hours",
@@ -27,21 +27,21 @@ export const certificates = [
       "Covered IoT value chain, application development using Master of Things, and hands-on training with IoT hardware.",
     image: new URL("src/assets/certificates/iot.png", import.meta.url).href,
     link: "#",
->>>>>>> 69650f0bb3eb81901eaa0c784099c7f62a657a73
+
   },
 
   {
     title: "Frontend Development - Vue.js",
     issuer: "Information Technology Institute (ITI)",
-<<<<<<< HEAD
+
     image: new URL("../assets/certificates/frontend.png", import.meta.url).href,
-=======
+
     date: "August 3 – September 1, 2025",
     duration: "120 hours",
     description:
       "Focused on client-side technologies, ECMAScript, HTML5, CSS3, Bootstrap, and Vue.js framework development.",
     image: new URL("src/assets/certificates/frontend.png", import.meta.url).href,
     link: "#",
->>>>>>> 69650f0bb3eb81901eaa0c784099c7f62a657a73
+
   }
 ];
